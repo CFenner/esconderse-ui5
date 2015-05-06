6f7b9bd1a969d2051709159bb9a0abe5c624141f
Christopher <Chrissi@Kleine-Freiheit.local>
2015-05-07 00:47:17 +0200

    added back method

--- src/view/Account.controller.js
+++ src/view/Account.controller.js
@@ -1,5 +1,10 @@
 jQuery.sap.require("de.esconderse.util.Formatter");
 
 sap.ui.core.mvc.Controller.extend("de.esconderse.view.Account", {
-	onInit: function(evt){}
+	onInit: function(evt){},
+	// ---------- navigation
+	onNavBack : function() {
+		// This is only relevant when running on phone devices
+		sap.ui.core.UIComponent.getRouterFor(this).myNavBack("main");
+	}
 });
