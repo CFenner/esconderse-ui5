e88fd92416b0d04df851779e10cb3993e80d994e
Christopher <Chrissi@Kleine-Freiheit.local>
2015-05-07 21:33:41 +0200

    refactor model loading

--- src/Component.js
+++ src/Component.js
@@ -4,45 +4,67 @@ jQuery.sap.require("de.esconderse.Router");
 sap.ui.core.UIComponent.extend("de.esconderse.Component", {
 	useMockData: false,
 	metadata : 'json',
+	register: function(bus){
+		bus.subscribe("master", "loadList", function(that){
+			return function(){ that.doLoadList(); };
+		}(this));
+		bus.subscribe("master", "loadAccount", function(that){
+			return function(){ that.doLoadAccount(); };
+		}(this));
+	},
     init : function() {
         sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
-        var mConfig = this.getMetadata().getConfig();
+        var mConfig = this.getMetadata().getConfig(), mailModel, resourceModel, deviceModel, accountModel;
         // always use absolute paths relative to our own component
         // (relative paths will fail if running in the Fiori Launchpad)
         var rootPath = jQuery.sap.getModulePath("de.esconderse");
-        // Create and set domain model to the component
-//        this.setModel(new sap.ui.model.odata.ODataModel(mConfig.serviceConfig.serviceUrl, true));
         
+		resourceModel = new sap.ui.model.resource.ResourceModel({
+			bundleUrl : [rootPath, mConfig.resourceBundle].join("/")
+        });
+        deviceModel = new sap.ui.model.json.JSONModel({
+        	isTouch : sap.ui.Device.support.touch,
+        	isNoTouch : !sap.ui.Device.support.touch,
+        	isPhone : sap.ui.Device.system.phone,
+        	isNoPhone : !sap.ui.Device.system.phone,
+        	listMode : sap.ui.Device.system.phone ? "None" : "SingleSelectMaster",
+        	listItemType : sap.ui.Device.system.phone ? "Active" : "Inactive"
+        });
+        deviceModel.setDefaultBindingMode("OneWay");
+        mailModel = new sap.ui.model.json.JSONModel();
+		mailModel.setDefaultBindingMode("OneWay");
+    	mailModel.attachRequestCompleted({}, function(){
+		    var msg = 'Liste aktualisiert';
+			sap.m.MessageToast.show(msg);
+			sap.ui.getCore().getEventBus().publish("master", "enableRefresh");
+    	});
+        accountModel = new sap.ui.model.json.JSONModel()
         // set i18n model
-        this.setModel(
-        	new sap.ui.model.resource.ResourceModel({
-            	bundleUrl : [rootPath, mConfig.resourceBundle].join("/")
-        	}), "i18n")
-        .setModel(
-        	new sap.ui.model.json.JSONModel({
-            	isTouch : sap.ui.Device.support.touch,
-            	isNoTouch : !sap.ui.Device.support.touch,
-            	isPhone : sap.ui.Device.system.phone,
-            	isNoPhone : !sap.ui.Device.system.phone,
-        		listMode : sap.ui.Device.system.phone ? "None" : "SingleSelectMaster",
-        		listItemType : sap.ui.Device.system.phone ? "Active" : "Inactive"
-        	})//.setDefaultBindingMode("OneWay")
-        	, "device")
-        .setModel(
-			new sap.ui.model.json.JSONModel(
-				this.useMockData
-				?"model/account.json"
-				:"https://esconderse.de/account"
-			)//.setDefaultBindingMode("OneWay")
-			, "account")
-		.setModel(
-			new sap.ui.model.json.JSONModel(
-				this.useMockData
-				?"model/mailListNew.json"
-				:"https://esconderse.de/ajax.php?list"
-			)//.setDefaultBindingMode("OneWay")
-		)
-		.getRouter()
-		.initialize();       
+        this.setModel(mailModel)
+        	.setModel(deviceModel, "device")
+        	.setModel(accountModel, "account")
+        	.setModel(resourceModel, "i18n")
+        	.doLoadList()
+        	.doLoadAccount()
+        	.register(sap.ui.getCore().getEventBus())
+			.getRouter()
+			.initialize();
+    },
+    onRefreshFinish: function(){
+    	this.getView();
+    },
+    doLoadList: function(){
+    	var model = this.getModel();
+    	model.loadData(
+			this.useMockData?"model/mailListNew.json":"https://esconderse.de/gateway.php?list"
+    	);
+    	return this;
+    },
+    doLoadAccount: function(){
+    	var model = this.getModel("account");
+    	model.loadData(
+			this.useMockData?"model/mailListNew.json":"https://esconderse.de/gateway.php?list"
+    	);
+    	return this;
     }
 });
