8948d021292f80434dbaa29fc2c935be27b4e565
Christopher <christopher.fenner@me.com>
2015-09-02 00:22:32 +0200

    move controller

---
+++ src/controller/fragment/DialogCreate.controller.js
@@ -0,0 +1,35 @@
+sap.ui.define(["sap.ui.core.mvc.Controller", "de.esconderse.util.Hektor"], function(Controller, Hector){
+	"use strict";
+	return Controller.extend("de.esconderse.controller.fragment.DialogCreate", {
+	/*	onInit: function(evt){
+
+		},
+	*/	// ---------- actions
+		onCreate: function(/*evt*/){
+			var description = this.getView().byId("createDescription").getValue(),
+				state = this.getView().byId("createState").getState();
+			Hektor.create(description, state, 
+				function(){
+					core.getEventBus().publish({}, "master", "enableCreate");
+				}, 
+				function(){
+				
+				});
+		},
+		onCancel: function(/*evt*/){
+			var core = sap.ui.getCore();
+			core.byId("dialogCreate").close();
+			core.getEventBus().publish({}, "master", "enableCreate");
+	/*	},
+		// ---------- navigation
+		onNavBack : function() {
+			// This is only relevant when running on phone devices
+			this._getRouter().myNavBack("main");
+		},
+		// ---------- private
+		_getRouter: function(){
+			return sap.ui.core.UIComponent.getRouterFor(this);
+	*/
+		}
+	});
+});
