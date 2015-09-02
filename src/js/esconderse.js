74878a816cd2cf2dfa65efd2b5efc85e59f42990
Christopher <christopher.fenner@me.com>
2015-09-02 21:36:41 +0200

    use sap.ui.define

--- src/js/esconderse.js
+++ src/js/esconderse.js
@@ -1,37 +1,41 @@
-navigator.id.watch({
-	loggedInUser: "null",
-	onlogin: function (assertion) {
+sap.ui.require([
+	"sap/m/MessageToast",
+	"sap/m/Shell",
+	"sap/ui/core/ComponentContainer"
+], function(Toast, Shell, Container){
 	"use strict";
-		$.ajax({
-			type: "POST",
-			url: "https://esconderse.de/verify",
-			data: {assertion: assertion},
-			success: function(/*data*/){
-				sap.ui.getCore().getEventBus().publish("master", "loadList");
-				sap.ui.getCore().getEventBus().publish("master", "loadAccount");
-			},
-			error: function(/*jqXHR, textStatus, errorThrown*/){
-				sap.m.MessageToast.show("Sie wurden abgemeldet!");
-				//TODO: call logout.php !!!!!!!!!
-				//window.location = "https://esconderse.de/logout.php";
-			},
-			dataType: "json"
-		});
-	},
-	onlogout: function () {
-	"use strict";
-		sap.m.MessageToast.show("Sie wurden abgemeldet!");
-		//TODO: call logout.php !!!!!!!!!
-		//window.location = "https://esconderse.de/logout.php";
-	}
-});
+	navigator.id.watch({
+		loggedInUser: "null",
+		onlogin: function (assertion) {
+			$.ajax({
+				type: "POST",
+				url: "https://esconderse.de/verify",
+				data: {assertion: assertion},
+				success: function(/*data*/){
+					sap.ui.getCore().getEventBus().publish("master", "loadList");
+					sap.ui.getCore().getEventBus().publish("master", "loadAccount");
+				},
+				error: function(/*jqXHR, textStatus, errorThrown*/){
+					Toast.show("Sie wurden abgemeldet!");
+					//TODO: call logout.php !!!!!!!!!
+					//window.location = "https://esconderse.de/logout.php";
+				},
+				dataType: "json"
+			});
+		},
+		onlogout: function () {
+			Toast.show("Sie wurden abgemeldet!");
+			//TODO: call logout.php !!!!!!!!!
+			//window.location = "https://esconderse.de/logout.php";
+		}
+	});
 
-sap.ui.getCore().attachInit(function() {
-	"use strict";
-	new sap.m.Shell({
-		app: new sap.ui.core.ComponentContainer({
-			height: "100%",
-			name: "de.esconderse"
-		})
-	}).placeAt("content");
+	sap.ui.getCore().attachInit(function() {
+		new Shell({
+			app: new Container({
+				height: "100%",
+				name: "de.esconderse"
+			})
+		}).placeAt("content");
+	});
 });
